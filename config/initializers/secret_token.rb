8b768f7a03abb8f0923f1ca7ada6e7e30da165fa
Lyric <llhupp09@smumn.edu>
2013-02-15 17:28:03 -0600

    -initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleAssociations::Application.config.secret_token = '3b5f63312cd694772223a6d6f0cc22892986458e3ab85dc9b1847a6fa968de6835001c46ed2ecd74e36331d9e5da15dfc2a415970c884da223e4f888246c02ec'
